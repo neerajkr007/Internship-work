5b6415733cdb2cd98b6da706c29f3d16e218da87
neerajkr007 <44816075+neerajkr007@users.noreply.github.com>
2021-02-11 13:55:39 +0530

    minor fixes

--- data/script.js
+++ data/script.js
@@ -1,28 +1,27 @@
-var correct_Answers = [["q1o1", "q2o4", "q3o2", "q4o2", "q5o3", "q6o4","q7o4","q8o4", "q9o1", "q0o2"]
+var correct_Answers = [
+                       ["q1o1", "q2o4", "q3o2", "q4o2", "q5o3", "q6o4","q7o4","q8o4", "q9o1", "q0o2"]
                       ,["q1o3", "q2o1", "q3o3", "q4o3", "q5o1", "q6o2","q7o1","q8o4", "q9o2", "q0o2"]
                       ,["q1o4", "q2o3", "q3o3", "q4o1", "q5o4", "q6o3","q7o2","q8o2", "q9o1", "q0o1"]
-                      ,["q1o1", "q2o1", "q3o4", "q4o4", "q5o3", "q6o1","q7o4","q8o1", "q9o1", "q0o3"]];
+                      ,["q1o1", "q2o1", "q3o4", "q4o4", "q5o3", "q6o1","q7o4","q8o1", "q9o1", "q0o3"]
+                      ];
 
 
 var markedCorrect = 0;
 var marked = 0;
-var red = [];
-var green = [];
 var quizNumber = 0;
 
 function optionchosen(option, n){
     document.getElementById("q"+n+"q"+option[1]).childNodes[1].childNodes[1].children[option[3]-1].onclick = null;
+    document.getElementById("q"+n+"q"+option[1]).childNodes[1].childNodes[1].children[option[3]-1].style.backgroundColor = "#444444";
     marked++;
     let options = document.getElementById("q"+n+"q"+option[1]).childNodes[1].childNodes[1].childElementCount
     for(var i in correct_Answers[n-1]){
         if(option == correct_Answers[n-1][i]){
-            document.getElementById("q"+n+"q"+option[1]).style.backgroundColor = "#bcf5bc";
-            //console.log(document.getElementById("q"+n+"q"+option[1]).childNodes[1].childNodes[1].childElementCount)
+            document.getElementById("q"+n+"q"+option[1]).style.backgroundColor = "#bcf5bc";//console.log(document.getElementById("q"+n+"q"+option[1]).childNodes[1].childNodes[1].childElementCount)
             for(let j = 0; j < options; j++){
                 document.getElementById("q"+n+"q"+option[1]).childNodes[1].childNodes[1].children[j].onclick = null;
                 document.getElementById("q"+n+"q"+option[1]).childNodes[1].childNodes[1].children[j].classList.remove("hov");
             }
-            //green.push("q"+n+"q"+option[1]);
             markedCorrect++;
             if(marked%10 == 0 && quizNumber != 3){
                 quizNumber++;
@@ -35,14 +34,14 @@ function optionchosen(option, n){
             }
             if(marked == 40){
                 if(markedCorrect <= 15){
-                    document.getElementById("modalBody").innerHTML = "<h5>Congratulations, Quiz completed !!!</h5>"+ "<h4>Your performance was<strong> below average</strong></h4>";
+                    document.getElementById("modalBody").innerHTML = "<h5>Congratulations, Quiz completed !!!</h5>"+ "<h4>Your performance was<strong> Below Average</strong></h4>";
                 }
                 else if(16 <= markedCorrect <= 25)
                 {
-                    document.getElementById("modalBody").innerHTML = "<h5>Congratulations, Quiz completed !!!</h5>"+ "<h4>Your performance was<strong> average</strong></h4>";
+                    document.getElementById("modalBody").innerHTML = "<h5>Congratulations, Quiz completed !!!</h5>"+ "<h4>Your performance was<strong> Average</strong></h4>";
                 }
                 else{
-                    document.getElementById("modalBody").innerHTML = "<h5>Congratulations, Quiz completed !!!</h5>"+ "<h4>Your performance was<strong> above average</strong></h4>";
+                    document.getElementById("modalBody").innerHTML = "<h5>Congratulations, Quiz completed !!!</h5>"+ "<h4>Your performance was<strong> Above Average</strong></h4>";
                 }
                 document.getElementById("cancel").innerHTML = "Close";
                 $('#Modal1').modal('toggle')
@@ -66,14 +65,14 @@ function optionchosen(option, n){
     }
     if(marked == 40){
         if(markedCorrect <= 15){
-            document.getElementById("modalBody").innerHTML = "<h5>Congratulations, Quiz completed !!!</h5>"+ "<h4>Your performance was<strong> below average</strong></h4>";
+            document.getElementById("modalBody").innerHTML = "<h5>Congratulations, Quiz completed !!!</h5>"+ "<h4>Your performance was<strong> Below Average</strong></h4>";
         }
         else if(16 <= markedCorrect <= 25)
         {
-            document.getElementById("modalBody").innerHTML = "<h5>Congratulations, Quiz completed !!!</h5>"+ "<h4>Your performance was<strong> average</strong></h4>";
+            document.getElementById("modalBody").innerHTML = "<h5>Congratulations, Quiz completed !!!</h5>"+ "<h4>Your performance was<strong> Average</strong></h4>";
         }
         else{
-            document.getElementById("modalBody").innerHTML = "<h5>Congratulations, Quiz completed !!!</h5>"+ "<h4>Your performance was<strong> above average</strong></h4>";
+            document.getElementById("modalBody").innerHTML = "<h5>Congratulations, Quiz completed !!!</h5>"+ "<h4>Your performance was<strong> Above Average</strong></h4>";
         }
         document.getElementById("cancel").innerHTML = "Close";
         $('#Modal1').modal('toggle')
@@ -81,13 +80,6 @@ function optionchosen(option, n){
 }
 
 function startQuiz(n){
-    // for(var i in red){
-    //     document.getElementById(red[i]).style.backgroundColor = "#FFFFFF";
-    // }
-    // for(var i in green){
-    //     document.getElementById(green[i]).style.backgroundColor = "#FFFFFF";
-    // }
-    //document.getElementById("quizButton"+n).disabled = true;
     if(n == 1){
         document.getElementById("quiz1").style.display = "block";
         document.getElementById("quiz2").style.display = "none";
