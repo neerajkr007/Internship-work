14c07fe6478425e81c7011b2d723749e5c66d288
neerajkr007 <44816075+neerajkr007@users.noreply.github.com>
2021-02-12 23:54:34 +0530

    beta for personality test

--- data/script.js
+++ data/script.js
@@ -12,6 +12,18 @@ var marked = [0,0,0,0];
 var quizCompleted = 0;
 var current_quiz = 0;
 var allDone = false;
+var quiz5Questions = ["My primary reason for trading ", "I regarded myself as a ", "I considered myself as a ", "At a social event do you:", "I read books and magazines about trading and investing:", "are you more impressed ", "are you more attracted to", "which appeal to you more :", "Are you more comfortable making:", "I have been trading for:", "Would you say you are more:", "Before making a phone call do you:", "I would be prefer to be trading ", "Are visionaries:", "Are you more often a:", "Should events occur:", "I currently make my living from:", "In company do you:", "Common sense is:", "In making decisions do you feel more comfortable with:", "Are you more:", "Do you prefer:", "do you put more value on :", "are you  more likely to :", "which rules you more", "I learn better:", "do you trend to look for :", "do you prefer:", "Are you interested in:", "I most often:", "Do you more often prefer the:", "Are you more likely to trust on", "Are you more inclined to be:", "What appeals to you more:"]
+var quiz5Options1 = ["Because I love doing it", "novice trader", "active trader", "interact with many, including strangers?", "Often", "principles?", "sensible people", "consistency of thought?", "logical judgments?", "5 years or less", "serious and determined", "rarely question that everything will be said?", "full time ", "somewhere annoying", "cool ", "be careful selection", "trading exclusive", "initiate conversation?", "rarely questionable?", "standards ", "firm than gentle", "the ability to organize and be methodical?", "the definite", "see how others are useful?", "your head ", "Studying at home by myself", "the orderly?", "many friends with brief contact?", "design and research?", "Make Money", "the tentative and preliminary?", "experience?", "easy to approach?", "the structured and scheduled?"]
+var quiz5Options2 = ["to make money ", "seaoned trade", "passive trader", 'be "stuck in a routine?"', "ocassionaly", "emotions", "imaginative people", "harmonious human relationships?", "value judgments?", "more than 5 years ", "easy going ", "rehearse what you will say?", "part time", "rather fascinating", "warm", "randomly", "something else", "wait to be approached?", "frequently questionbale", "feelings", "gentle than firm?", "the ability to adapt and make do", "the open minded ", "see how others see?", "your heart ?", "In a group with other people", "whatever turns ups", "a few friends with more lengthy contact?", "production and distribution?", "Lose Money", "final and unalterable?", "gut instinct?", "somewhat reserved?", "the unstructured and unscheduled?"]
+var submision = new Array(34);
+for(let i = 0; i < 34; i++){
+    submision[i] = new Array(1);
+    submision[i][0] = -1;
+}
+var type = [[1,1,1,1,1,0,0,0,1,0,1,0,1,1,-2,0,-2,1,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0],
+            [0,0,0,0,0,1,1,1,0,1,0,1,0,0,1,1,0,0,1,1,1,-2,1,1,1,1,1,0,1,1,0,1,1,1],
+            [1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,0,0,1,1,0,1,1,1,1,1,0,0,0,0,1,0,0,0,0]];
+var similar = [0,0,0];
 
 
 
@@ -144,26 +156,17 @@ function startNextQuiz(){
 
 
 function optionchosen2(option, n){
-    for(let i = 0; i < document.getElementById("q"+n+"q"+option[1]).children[0].children.length; i++){
-        if(i == option[3]-1){
-            document.getElementById("q"+n+"q"+option[1]).children[0].children[i].style.backgroundColor = "#444444";
-            
+    document.getElementById("q"+n+"q"+option[1]+option[2]).style.backgroundColor = "#FFFFFF"
+    for(let i = 0; i < document.getElementById("q"+n+"q"+option[1]+option[2]).children[0].children.length; i++){
+        if(i == option[4]-1){
+            document.getElementById("q"+n+"q"+option[1]+option[2]).children[0].children[i].style.backgroundColor = "#444444";
         }
         else{
-            document.getElementById("q"+n+"q"+option[1]).children[0].children[i].style.backgroundColor = "gray";
+            document.getElementById("q"+n+"q"+option[1]+option[2]).children[0].children[i].style.backgroundColor = "gray";
         }
     }
-
-
-    //console.log(document.getElementById("q"+n+"q"+option[1]).childNodes[1].childNodes[1].children[i].style.backgroundColor == "rgb(68, 68, 68)");
-
-
-
 }
 
-var quiz5Questions = ["My primary reason for trading ", "I regarded myself as a ", "I considered myself as a ", "At a social event do you:", "I read books and magazines about trading and investing:", "are you more impressed ", "are you more attracted to", "which appeal to you more :", "Are you more comfortable making:", "I have been trading for:", "Would you say you are more:", "Before making a phone call do you:", "I would be prefer to be trading ", "Are visionaries:", "Are you more often a:", "Should events occur:", "I currently make my living from:", "In company do you:", "Common sense is:", "In making decisions do you feel more comfortable with:", "Are you more:", "Do you prefer:", "do you put more value on :", "are you  more likely to :", "which rules you more", "I learn better:", "do you trend to look for :", "do you prefer:", "Are you interested in:", "I most often:", "Do you more often prefer the:", "Are you more likely to trust on", "Are you more inclined to be:", "What appeals to you more:"]
-var quiz5Options1 = ["Because I love doing it", "novice trader", "active trader", "interact with many, including strangers?", "Often", "principles?", "sensible people", "consistency of thought?", "logical judgments?", "5 years or less", "serious and determined", "rarely question that everything will be said?", "full time ", "somewhere annoying", "cool ", "be careful selection", "trading exclusive", "initiate conversation?", "rarely questionable?", "standards ", "firm than gentle", "the ability to organize and be methodical?", "the definite", "see how others are useful?", "your head ", "Studying at home by myself", "the orderly?", "many friends with brief contact?", "design and research?", "Make Money", "the tentative and preliminary?", "experience?", "easy to approach?", "the structured and scheduled?"]
-var quiz5Options2 = ["to make money ", "seaoned trade", "passive trader", 'be "stuck in a routine?"', "ocassionaly", "emotions", "sensible people", "harmonious human relationships?", "value judgments?", "more than 5 years ", "easy going ", "rehearse what you will say?", "part time", "rather fascinating", "warm", "randomly", "something else", "wait to be approached?", "frequently questionbale", "feelings", "gentle than firm?", "the ability to adapt and make do", "the open minded ", "see how others see?", "your heart ?", "In a group with other people", "whatever turns ups", "a few friends with more lengthy contact?", "production and distribution?", "Lose Money", "final and unalterable?", "gut instinct?", "somewhat reserved?", "the unstructured and unscheduled?"]
 
 function enterQuestion(qid, question, option1, option2)
 {
@@ -172,21 +175,18 @@ function enterQuestion(qid, question, option1, option2)
     li.setAttribute("id", qid);
     li.setAttribute("class", "list-group-item");
     li.setAttribute("style", "max-width: 80vw;");
-    if(qid.length == 4)
-        li.appendChild(document.createTextNode(qid[3]+". "+question));
-    else
     li.appendChild(document.createTextNode(qid[3]+qid[4]+". "+question));
     var ul = document.createElement('ul');
     ul.setAttribute("class", "list-group mt-3");
     var li2 = document.createElement('li');
     li2.setAttribute("class", "list-group-item option_Style hov text-white")
     li2.setAttribute("style", "max-width: 300px !important;");
-    li2.setAttribute("onclick", "optionchosen2('q"+qid[3]+"o1', 5)");
+    li2.setAttribute("onclick", "optionchosen2('q"+qid[3]+qid[4]+"o1', 5)");
     li2.appendChild(document.createTextNode(option1));
     var li3 = document.createElement('li');
     li3.setAttribute("class", "list-group-item option_Style hov text-white")
     li3.setAttribute("style", "max-width: 300px !important;");
-    li3.setAttribute("onclick", "optionchosen2('q"+qid[3]+"o2', 5)");
+    li3.setAttribute("onclick", "optionchosen2('q"+qid[3]+qid[4]+"o2', 5)");
     li3.appendChild(document.createTextNode(option2));
     ul.appendChild(li2);
     ul.appendChild(li3);
@@ -194,7 +194,88 @@ function enterQuestion(qid, question, option1, option2)
     _question.appendChild(li);
 }
 
+
 function testi(){
+    for(let i = 0; i < 34; i++){
+        if(i<9)
+            enterQuestion("q5q0"+(i+1), quiz5Questions[i], quiz5Options1[i], quiz5Options2[i]);
+        else
+            enterQuestion("q5q"+(i+1), quiz5Questions[i], quiz5Options1[i], quiz5Options2[i]);
+    }
+//     var submit = document.createElement('button');
+//     submit.setAttribute("class", "btn btn-success mt-3");
+//     submit.appendChild(document.createTextNode("Submit"));
+//     document.getElementById("quiz5").appendChild(submit);
+}
+
+
+function personality()
+{
+    let test = false;
     for(let i = 0; i < 34; i++)
-    enterQuestion("q5q"+(i+1), quiz5Questions[i], quiz5Options1[i], quiz5Options2[i]);
+    {
+        if(i<9){
+            if(document.getElementById("q5q0"+(i+1)).children[0].children[0].style.backgroundColor == "rgb(68, 68, 68)"){
+                submision[i][0] = 0;
+            }
+            else if(document.getElementById("q5q0"+(i+1)).children[0].children[1].style.backgroundColor == "rgb(68, 68, 68)")
+                submision[i][0] = 1;
+            else{
+                document.getElementById("q5q0"+(i+1)).style.backgroundColor = "#ee9090";
+                document.getElementById("modalTitle").innerHTML = "Warning !";
+                document.getElementById("modalBody").innerHTML = "Please complete the whole quiz";
+                document.getElementById("cancel").onclick = "$('#Modal1').modal('toggle')";
+                document.getElementById("cancel").innerHTML = "Okay";
+                $('#Modal1').modal('toggle')
+                test = true;
+            }
+        }
+        else{
+            if(document.getElementById("q5q"+(i+1)).children[0].children[0].style.backgroundColor == "rgb(68, 68, 68)"){
+                submision[i][0] = 0;
+            }
+            else if(document.getElementById("q5q"+(i+1)).children[0].children[1].style.backgroundColor == "rgb(68, 68, 68)")
+                submision[i][0] = 1;
+            else{
+                document.getElementById("q5q"+(i+1)).style.backgroundColor = "#ee9090";
+                document.getElementById("modalTitle").innerHTML = "Warning !";
+                document.getElementById("modalBody").innerHTML = "Please complete the whole quiz";
+                document.getElementById("cancel").onclick = "$('#Modal1').modal('toggle')";
+                document.getElementById("cancel").innerHTML = "Okay";
+                $('#Modal1').modal('toggle')
+                test = true;
+            }
+        }
+    }
+    if(test){
+        document.getElementById("personality_quiz_submit").disabled = true;
+        document.getElementById("modalTitle").innerHTML = "Result";
+        document.getElementById("modalBody").innerHTML = '<div class="d-flex align-items-center"><strong>Please wait while we calculate your result...</strong><div class="spinner-border ml-auto" role="status" aria-hidden="true"></div></div>';
+        //document.getElementById("cancel").onclick = "$('#Modal1').modal('toggle')";
+        document.getElementById("cancel").style.display = "none";
+        $('#Modal1').modal('toggle')
+        for(let i = 0; i < 3; i++)
+        {
+            for(let j = 0; j < 34; j++)
+            {
+                if(submision[j][0] == type[i][j]){
+                    console.log(i+" "+j+" "+submision[j][0])
+                    similar[i]++;
+                }
+            }
+        }
+        console.log(similar)
+        if(similar[0] >= similar[1] && similar[0] >= similar[2]) {
+            document.getElementById("modalBody").innerHTML = "You are a ADMINISTRATIVE Trader";
+        }
+        else if (similar[1] >= similar[0] && similar[1] >= similar[2]) {
+            document.getElementById("modalBody").innerHTML = "You are a SPONTANEOUS Trader";
+        }
+        else {
+            document.getElementById("modalBody").innerHTML = "You are a FacilITATIVE Trader";
+        }
+        document.getElementById("cancel").style.display = "block";
+        //document.getElementById("cancel").onclick = "$('#Modal1').modal('toggle')";
+    }
+    
 }
